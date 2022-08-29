52b9292fea3a174a6d4749509215ecc64babb6c4
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-29 16:54:04 -0500

    added text and boxes to about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -6,11 +6,40 @@ function AboutPage() {
   
   
   return (
-    <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
+  
+    <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8 sm:py-24 text-center">
+      <h2 className="text-3xl text-white font-bold sm:text-4xl  p-6 bg-[#14195E]">
+        Our Process
+      </h2>
+
+      <div className="lg:py-16 bg-[#ced1f4]">
+        <article className="space-y-4 text-gray-600">
+          <h3 className="font-bold">
+            Step 1. 
+          </h3>
+          <p>You complete our quick form and give us information about your current insurance plan</p>
+          <h3 className="font-bold">
+            Step 2. 
+          </h3>
+          <p>We use that infomation to match you with the financial pros that are right for you</p>
+          <h3 className="font-bold">
+            Step 3. 
+          </h3>
+          <p>Your agents will reach out to you within X business days with options to save you money and better your financial wellness</p>
+        </article>
       </div>
-    </div>
+
+        <h2 className="text-3xl text-white font-bold sm:text-4xl p-6 bg-[#14195E]">
+          Why work with us?
+        </h2>
+        <div className="lg:py-16 bg-[#ced1f4]">
+        <article className="space-y-4 text-gray-600">
+          <p>Who is BlueVest Pro?</p>
+          <p>What sets BlueVest Pro apart?</p>
+        </article>
+        </div>
+  </div>
+
   );
 }
 
