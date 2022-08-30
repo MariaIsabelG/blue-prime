725b30183dd0f10f0c701e51a72c46c5d01440f8
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-30 14:56:04 -0500

    added borders and spacing to the boxes

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -7,14 +7,14 @@ function AboutPage() {
   
   return (
   
-    <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8 sm:py-24 text-center">
-      <h2 className="text-3xl text-white font-bold sm:text-4xl p-6 bg-[#14195E]">
+    <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8 sm:py-24 text-center ">
+      <h2 className="text-3xl text-white font-bold sm:text-4xl p-6 bg-blue-600 border rounded-t-xl xl:mx-36">
         Our Process
       </h2>
 
-      <div className="lg:py-16 bg-[#ced1f4]">
-        <article className="space-y-4 text-gray-600">
-          <h3 className="font-bold">
+      <div className="lg:py-0 bg-[#d6e2ed] border rounded-b-xl mb-5 xl:mx-36">
+        <article className="space-y-4 text-black mb-5 px-12">
+          <h3 className="font-bold mt-5 lg:mt-5">
             Step 1. 
           </h3>
           <p>You complete our quick form and give us information about your current insurance plan</p>
@@ -29,11 +29,13 @@ function AboutPage() {
         </article>
       </div>
 
-        <h2 className="text-3xl text-white font-bold sm:text-4xl p-6 bg-[#14195E]">
+
+
+        <h2 className="text-3xl text-white font-bold sm:text-4xl p-6 bg-blue-600 border rounded-t-xl xl:mx-36">
           Why work with us?
         </h2>
-        <div className="lg:py-16 bg-[#ced1f4]">
-        <article className="space-y-4 text-gray-600">
+        <div className="lg:py-16 bg-[#d6e2ed] border rounded-b-xl xl:mx-36">
+        <article className="space-y-4 text-black">
           <p>Who is BlueVest Pro?</p>
           <p>What sets BlueVest Pro apart?</p>
         </article>
