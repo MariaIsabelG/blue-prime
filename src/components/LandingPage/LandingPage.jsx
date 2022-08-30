888cac01a62d66c115919f1a25d9f3b74309b5fb
Rachel Bruce <rachelbruce922@gmail.com>
2022-08-30 16:26:58 -0500

    changed picture and layout

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -17,7 +17,7 @@ function LandingPage() {
     <div>
 
       {/* FIND A PRO SECTION */}
-      <section>
+      {/* <section>
         <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
             <div
@@ -65,7 +65,51 @@ function LandingPage() {
           </div>
         </div>
       </div>
-</section>
+</section> */}
+
+<div className='flex overflow-hidden bg-white'>
+                <div className='pt-16 pb-20 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48'>
+                    <div className=' mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8'>
+                        <div className='sm:max-w-lg'>
+                            <h1 className='font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+                                Summer styles are finally here
+                            </h1>
+                            <p className='mt-4 text-xl text-gray-500'>
+                                This year, our new summer collection will shelter you from the harsh elements of a
+                                world that doesn't care if you live or die.
+                            </p>
+                        </div>
+                        <div>
+                            <div className='mt-10'>
+                                {/* Decorative image grid */}
+                                <div
+                                    aria-hidden='true'
+                                    className='pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl'>
+                                    <div className='md:absolute md:transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8'>
+                                        <div className='flex items-center space-x-6 lg:space-x-8'>
+                                            <div className='grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8'>
+                                                <img
+                                                    src='images/piggy_bank.jpg'
+                                                    alt=''
+                                                    className='h-full w-full object-cover object-center'
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a
+                                    href='#'
+                                    className='inline-block rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-center font-medium text-white hover:bg-indigo-700'>
+                                    Shop Collection
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
 
 {/* RESOURCES SECTION */}
 <section className="text-white bg-blue-900">
