aa91d49efe59b6baf7420dc204a4deb0e09dca37
Rachel Bruce <rachelbruce922@gmail.com>
2022-08-31 14:13:09 -0500

    rendering client information on page

--- src/components/AgentDashboard/AgentDashboard.jsx
+++ src/components/AgentDashboard/AgentDashboard.jsx
@@ -3,6 +3,8 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux"
 import { useHistory } from "react-router-dom";
 
+import AgentLeads from "../AgentLeads/AgentLeads";
+
 function AgentDashboard() {
 
     const dispatch = useDispatch();
@@ -11,7 +13,7 @@ function AgentDashboard() {
     const agent = useSelector(store => store.user);
     console.log('agent id', agent.id);
 
-    const clientList = useSelector(store => store.clients);
+    const clientList = useSelector(store => store.clients.clientList);
     console.log('clientList', clientList);
 
     useEffect(() => {
@@ -20,7 +22,20 @@ function AgentDashboard() {
 
 return(
     <>
-        
+        {/* <AgentLeads /> */}
+        {clientList.map(client => {
+            return (
+                <div key={client.id}>
+                    <div>
+                        <h5>{client.first_name} {client.last_name}</h5>
+                        <h6>Matched on: {client.matched_date}</h6>
+                        <p>{client.email}</p>
+                        <p>{client.phone_number}</p>
+                        <p>{client.state} {client.zip_code}</p>
+                    </div>
+                </div>
+        )
+        })}
     </>
 )
 }
