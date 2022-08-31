3ca634a27888b8346a38544bb6a84a42c77207e3
Rachel Bruce <rachelbruce922@gmail.com>
2022-08-31 11:01:00 -0500

    set up client saga and added to root saga

---
+++ src/redux/sagas/client.saga.js
@@ -0,0 +1,17 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* getClients(action) {
+    try {
+
+    }
+    catch(error) {
+        console.log('error in getClients', error);
+    }
+}
+
+function clientSaga() {
+    yield takeLatest('GET_CLIENTS', getClients)
+}
+
+export default clientSaga
