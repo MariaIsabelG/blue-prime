c4bd7d9c01fa48f0fe90c01a439097624f18468d
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-08-31 13:09:25 -0500

    added register page to login page and component

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+import RegisterPage from '../RegisterPage/RegisterPage';
 
 function LoginPage() {
   const history = useHistory();
 
   return (
     <div>
-      <div className="flex flex-row justify-center items-center" >
-        <LoginForm />
-      </div>
+      <div>
+        <div className="flex flex-row justify-center items-center" >
+          <LoginForm />
+        </div>
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/registration');
-          }}
-        >
-          Register
-        </button>
-      </center>
+        {/* <center>
+          <button
+            type="button"
+            className="btn btn_asLink"
+            onClick={() => {
+              history.push('/registration');
+            }}
+          >
+            Register
+          </button>
+        </center> */}
+      </div>
+    <div>
+        <RegisterPage />
     </div>
+    </div>
+
   );
 }
 
