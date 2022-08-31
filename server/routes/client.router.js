0fa82d8161693a09df2d9f8ad52fc3fbb91000d8
Rachel Bruce <rachelbruce922@gmail.com>
2022-08-31 15:58:02 -0500

    added functionality to select a specific client

--- server/routes/client.router.js
+++ server/routes/client.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-
+//GET clients for a specific agent
 router.get('/:id', (req, res) => {
     const id = req.params.id
     let queryText = `SELECT * FROM "client" 
@@ -17,6 +17,23 @@ router.get('/:id', (req, res) => {
         res.sendStatus(500);
     })
 });
+
+
+//GET a specific client
+router.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    let queryText = `SELECT * FROM "client" 
+    JOIN "client_user" ON "client"."id" = "client_user"."client_id" 
+    WHERE "client"."id" = ${id};`
+    pool.query(queryText)
+    .then(result => {
+        res.send(result.rows)
+    })
+    .catch(error => {
+        console.log('error getting a specific client', error)
+        res.sendStatus(500);
+    }) 
+})
 
 /**
  * POST route template
--- src/components/Agent Potentials/AgentPotentials.jsx
+++ src/components/Agent Potentials/AgentPotentials.jsx
@@ -1,9 +1,16 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import AgentDashboard from "../AgentDashboard/AgentDashboard";
 
 function AgentPotentials() {
 
+    const dispatch = useDispatch();
+
     const clientList = useSelector(store => store.clients.clientList);
+
+    const handleClientClick = (clientId) => {
+        console.log('clientId', clientId)
+        dispatch({type: 'GET_THIS_CLIENT', payload: clientId})
+    };
 
     return(
         <>
@@ -15,6 +22,7 @@ function AgentPotentials() {
                         <div key={client.id}>
                             <div
                                 className="block p-8 m-9 transition border border-gray-200 shadow-xl rounded-xl hover:shadow-blue-600/10 hover:border-blue-800/10"
+                                onClick={() => handleClientClick(client.id)}
                             >
                                 <h5>{client.first_name} {client.last_name}</h5>
                                 <h6>Matched on: {client.matched_date}</h6>
