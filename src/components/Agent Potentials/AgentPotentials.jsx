c336bc2c7672a84088500979b3600de8db8eac66
Rachel Bruce <rachelbruce922@gmail.com>
2022-09-01 13:44:05 -0500

    got the onChange function to work in the select input to change status

--- src/components/Agent Potentials/AgentPotentials.jsx
+++ src/components/Agent Potentials/AgentPotentials.jsx
@@ -19,9 +19,11 @@ function AgentPotentials() {
         history.push(`/client/${clientId}`)
     };
 
-    const handleStatusUpdate = (status) => {
-        console.log('status', status)
-    };
+    const handleStatusChange = (event) => {
+        setNewStatus(event.target.value)
+        console.log('newStatus', newStatus)
+
+    }
 
     return(
         <>
@@ -40,21 +42,23 @@ function AgentPotentials() {
                                 <p>{client.email}</p>
                                 <p>{client.phone_number}</p>
                                 <p>{client.state} {client.zip_code}</p>
+                                <h3>{client.status}</h3>
+                                <h3>{newStatus}</h3>
 
                                 <div className="mt-8">
                                     <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
                                         Update Status
                                     </label>
 
-                                    <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                        <option value="1" >Lead</option>
-                                        <option value="2" >Potential</option>
-                                        <option value="3" >Won</option>
-                                        <option value="4" >Lost</option>
+                                    <select value={newStatus} onChange={handleStatusChange} id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option key={1} value="1" >Lead</option>
+                                        <option key={2} value="2" >Potential</option>
+                                        <option key={3} value="3" >Won</option>
+                                        <option key={4} value="4" >Lost</option>
                                     </select>
                                     <button 
 									className='mt-2 inline-block rounded-md border border-transparent bg-blue-600 py-3 px-8 text-center font-medium text-white hover:bg-blue-700'
-                                    onClick={() => handleStatusUpdate(status)}
+                                    onClick={() => handleStatusUpdate(newStatus)}
                                     >Update</button>
                                 </div>
                             </div>
