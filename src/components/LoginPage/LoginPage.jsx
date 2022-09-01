6ce0fecf9a7727ab4fb8d4a5e9a0d98f887b8a2d
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-09-01 16:13:57 -0500

    styling

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -3,12 +3,15 @@ import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
+// LEFT OFF ---- Add padding for both lists
+// Complete Register From and link for popup log in
+
 function LoginPage() {
   const history = useHistory();
 
   return (
     <section class="h-full">
-      <div className="md:pt-32 md:pb-32 sm:pt-10 sm:pb-10 md:pt-10 md:pb-10 pb-10 pt-10 bg-orange-200  mb-16">
+      <div className="md:pt-32 md:pb-32 sm:pt-10 sm:pb-10 md:pt-10 md:pb-10 lg:pt-8 lg:pb-2 pb-10 pt-10 bg-orange-200 mb-2">
         <h3 className="text-xl text-center text-black mb-4">Why work with us? </h3>
           <li className="text-center text-black mb-1">We're your strategic growth partner</li>
           <li className="text-center text-black mb-1">Financial professionals spend a bulk of their time prospecting.</li>
@@ -20,30 +23,19 @@ function LoginPage() {
           <img className="w-3/12  sm:w-3/12 md:w-3/12 lg:w-3/12 xl:w-3/12" src="images/NW-Mutual.png"/>
         </div>
       </div>
-      <div class="px-8 h-full text-gray-800">
-        <div class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
-          <div class="grow-0 shrink-1 md:shrink-1 basis-auto 2xl:w-5/12 xl:w-6/10 lg:w-6/12 md:w-7/12 md:mb-6">
-            <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-              class="w-full"
-              alt="Sample image"
-            />
-          </div>
-        <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 md:mb-0">
-            <LoginForm/>
-          <div
-            class="flex items-center before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
-          >
-            <p class="text-center text-black font-semibold mx-4 mb-0">OR</p>
-          </div>
-
-
-          <div class="">
-            <RegisterForm/>
-          </div>
+      <div className="md:pt-32 md:pb-32 sm:pt-10 sm:pb-10 md:pt-10 md:pb-10 lg:pt-8 lg:pb-2 pb-10 pt-10 bg-white mb-8">
+        <h3 className="text-xl text-center text-black mb-4">What you can Expect when you Sign Up:</h3>
+          <li className="text-center text-black mb-1">BlueVest's team is your partner to grow your business. Upon signing up for BlueVest Pro, you'll be introduced to a personal Account Manager that will help you establish a game plan, provide strategic advice, maximize ROI, and develop a custom roadmap based on your specific business needs and budget.</li>
+          <li className="text-center text-black mb-1">We're committed to providing you with high-quality consumer prospects, account flexibility and excellent customer service.</li>
       </div>
-    </div>
-  </div>
+      <div class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+        <div class="xl:ml-20 xl:w-3/12 lg:w-5/12 md:w-8/12 md:mb-0">
+          <LoginForm/>
+        </div>
+        <div class="xl:ml-20 xl:w-3/12 lg:w-5/12 md:w-8/12 md:mb-0">
+          <RegisterForm/>
+        </div>
+      </div>
 </section>
 
   );
