b67e07f1422ce488f808adc6febff0caa9673c80
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-09-01 10:20:25 -0500

    adding why work with us

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -7,15 +7,16 @@ function LoginPage() {
   const history = useHistory();
 
   return (
-    <div className="w-screen bg-white">
-        <div className="">
-          <h3 className="text-2xl text-white text-center font-bold sm:text-1xl p-6">Why work with us?</h3>
-            <ol type="1" className="text-center text-white ">
-              <li>We're your strategic growth partner</li>
-            </ol>
-
-        </div>
-        <div>
+    <div className="w-screen h-full bg-white">
+      <div className="mt-6 bg-orange-200">
+        <h3 className="text-xl text-center text-black">Why work with us? </h3>
+            <li className="text-center text-black">We're your strategic growth partner</li>
+            <li className="text-center text-black">Financial professionals spend a bulk of their time prospecting.</li>
+            <li className="text-center text-black">By working with us, you'll spend less time marketing yourself, and more time advising your clients.</li>
+        <h3 className="text-xl text-center text-black">BlueVest has helped some of the biggest names in financial services grow their brand and practice.</h3>
+          <img></img>
+      </div>
+      <div>
           <LoginForm />
         </div>
         
