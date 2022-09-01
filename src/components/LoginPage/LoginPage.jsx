3e1a66dd634f3aa04512387ef0cf91b14b49acc4
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-09-01 12:08:23 -0500

    added imgs

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -9,12 +9,14 @@ function LoginPage() {
   return (
     <div className="w-screen h-full bg-white">
       <div className="mt-6 bg-orange-200">
-        <h3 className="text-xl text-center text-black">Why work with us? </h3>
-            <li className="text-center text-black">We're your strategic growth partner</li>
-            <li className="text-center text-black">Financial professionals spend a bulk of their time prospecting.</li>
-            <li className="text-center text-black">By working with us, you'll spend less time marketing yourself, and more time advising your clients.</li>
-        <h3 className="text-xl text-center text-black">BlueVest has helped some of the biggest names in financial services grow their brand and practice.</h3>
-          <img></img>
+        <h3 className="text-xl text-center text-black mb-4">Why work with us? </h3>
+            <li className="text-center text-black mb-1">We're your strategic growth partner</li>
+            <li className="text-center text-black mb-1">Financial professionals spend a bulk of their time prospecting.</li>
+            <li className="text-center text-black mb-1">By working with us, you'll spend less time marketing yourself, and more time advising your clients.</li>
+        <h3 className="text-xl text-center text-black mt-6">BlueVest has helped some of the biggest names in financial services grow their brand and practice.</h3>
+          <div className="bg-transparent">
+          <img className="w-32"src={FarmersInsurance}></img>
+          </div>
       </div>
       <div>
           <LoginForm />
