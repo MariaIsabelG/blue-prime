b31bbcf023ecf2f3669d94f4517f97b65c38b4ad
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-09-05 12:22:18 -0500

    dashboard

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -80,7 +80,7 @@ function Nav() {
 								<li
 									onClick={() => history.push('/agent')}
 									className='cursor-pointer hover:text-white'>
-									Agent
+									Dashboard
 								</li>
 							) : (
 								''
