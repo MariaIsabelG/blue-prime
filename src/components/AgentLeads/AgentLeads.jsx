55f92c1d965c41f1bc828b7394b37eb1ca69fd8c
Rachel Bruce <rachelbruce922@gmail.com>
2022-09-06 10:01:18 -0500

    changed value to dfault value on status select input. Prevents all inputs from changing at the same time

--- src/components/AgentLeads/AgentLeads.jsx
+++ src/components/AgentLeads/AgentLeads.jsx
@@ -31,29 +31,30 @@ function AgentLeads() {
             {clientList.map(client => {
                 if (client.status === 1) {
                     return (
-                        <div key={client.id}>
+                                
+                        <div key={client.id} >
                             <div
-                                className="block p-8 m-9 transition border border-gray-200 shadow-xl rounded-xl hover:shadow-blue-600/10 hover:border-blue-800/10"
+                                className="sm:px-6 lg:px-8 p-8 m-2 transition border border-gray-200 shadow-xl rounded-xl hover:shadow-blue-600/10 hover:border-blue-800/10 "
                             >
-                                <h5>{client.first_name} {client.last_name}</h5>
+                                <h5 className="text-center">{client.first_name} {client.last_name}</h5>
                                 <h6>Matched on: {client.matched_date}</h6>
                                 <p>{client.email}</p>
                                 <p>{client.phone_number}</p>
                                 <p>{client.state} {client.zip_code}</p>
 
                                 <div className="mt-8">
-                                    <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+                                    <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
                                         Update Status
                                     </label>
 
-                                    <select value={newStatus} onChange={handleStatusChange} id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <select key={client.id} onChange={handleStatusChange} id="status" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                         <option key={1} value="1" >Lead</option>
                                         <option key={2} value="2" >Potential</option>
                                         <option key={3} value="3" >Won</option>
                                         <option key={4} value="4" >Lost</option>
                                     </select>
                                     <button 
-                                    className='mt-2 inline-block rounded-md border border-transparent bg-blue-600 py-3 px-8 text-center font-medium text-white hover:bg-blue-700'
+                                    className='mt-2 inline rounded-md border border-transparent bg-blue-600 py-3 px-8 text-center font-medium text-white hover:bg-blue-700'
                                     onClick={() => handleStatusUpdate(newStatus, client.client_id)}
                                     >Update</button>
                                 </div>
