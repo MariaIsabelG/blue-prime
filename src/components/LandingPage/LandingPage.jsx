d7aa8d02fe468b0908f5ad172506ca0ca88ca9e9
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-09-06 18:05:10 -0500

    chanages

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -16,22 +16,32 @@ function LandingPage() {
 	return (
 		<div>
 			<div className='w-full mt-10 sm:mt-0 md:mt-0 lg:mt-0 '>
-			<div className='sm:flex md:flex'>
-				<div className="sm:static md:static lg:pl-36 xl:mt-20">
-					<div className='px-10 sm:mt-24 md:mt-24 lg:mt-30 text-4xl lg:text-7xl xl:7xl text-gray-900'>
+			<div className='sm:flex md:flex bg-white'>
+				<div className="sm:static md:static lg:pl-36 xl:mt-10">
+					<div className='px-10 text-4xl text-gray-900 sm:mt-24 md:mt-24 lg:mt-30 lg:text-7xl xl:mt-16 xl:7xl '>
 						<h1 className="font-bold">
 							Find Your Financial Professional
 						</h1>
-						<p className='mt-4 text-xl text-gray-500'>Where your time and and insurance needs are valued</p>
-						<p className='mt-4 text-xl text-gray-500'>
-							Answer a few short quick questions and get connected with us!
-						</p>
+						<li className='mt-10 text-xl text-gray-500'>Where your time and and insurance needs are valued.</li>
+						<li className='mt-4 text-xl text-gray-500'>We provide a convenient way to find financial professionals in your area.</li>
+						<li className='mt-4 text-xl text-gray-500'> Answer a few short quick questions and get connected with us!</li>
 					</div>
-					<div onClick={() => history.push('/findPro')} className='flex items-center justify-center inline-block border-2 rounded-lg bg-green-600 text-white hover:shadow-lg hover:bg-green-700 w-52 h-16 text-3xl mb-10 mt-8 ml-10'>
-						Find Your Pro
+					<div className="flex">
+						<div className="static">
+						<div onClick={() => history.push('/findPro')} className='flex items-center justify-center inline-block border-2 rounded-lg bg-green-500 text-white hover:shadow-lg hover:bg-green-700 w-48 h-16 text-2xl mb-10 mt-8 ml-10'>
+							Find Your Pro
+						</div>
+						</div>
+					
+					<div onClick={() => history.push('/about')} className='flex items-center justify-center inline-block border-2 rounded-lg bg-orange-400 text-white hover:shadow-lg hover:bg-orange-700 w-52 h-16 text-2xl mb-10 mt-8 ml-10'>
+						Get to know us!
+					</div>
+					<div onClick={() => history.push('/resources')} className='flex items-center justify-center inline-block border-2 rounded-lg bg-blue-400 text-white hover:shadow-lg hover:bg-blue-700 w-48 h-16 text-2xl mb-10 mt-8 ml-10 mr-10' >
+						Resources 
+					</div>
 					</div>
 				</div>
-				<div className='mt-0 sm:inline-block sm:w-6/12 md:w-6/12 md:inline-block lg:w-6/12 xl:w-6/12 ' aria-hidden='true'>
+				<div className='mt-0 sm:inline-block sm:w-6/12 md:w-6/12 md:inline-block lg:w-6/12 xl:w-7/12' aria-hidden='true'>
 					<img
 						src='images/piggy_bank.jpg'
 						alt=''
@@ -40,73 +50,85 @@ function LandingPage() {
 				</div>
 			</div>
 				</div>
+				<div className='pb-12 pt-10 bg-blue-600'>
+				<h2 className='text-center text-3xl text-white font-bold sm:text-1xl mb-6 '>
+					Our Process
+				</h2>
+				<div className='border rounded-md border mx-32 mb-4 bg-orange-200 border-2 border-gray-400'>
+					<div className='mb-4 mt-4 inline-block'>
+						<img src="images/formicon.png" className='font-bold text-2xl text-white w-16 ml-6 mr-6 '/>
+					</div>
+					<div className='inline-block text-2xl xl:text-xl'>
+						<p >
+							You complete our quick form and give us information about your current insurance plan
+						</p>
+					</div>
+				</div>
+				<div className='border rounded-md mx-32 mb-4 bg-orange-200 border-2 border-gray-400'>
+					<div className='mb-4 mt-4 inline-block'>
+						<img src="images/connecticon.png" className='font-bold text-2xl text-white w-16 ml-6 mr-6 '/>
+					</div>
+					<div className='inline-block text-2xl xl:text-xl'>
+						<p>We use that information to match you with the financial pros that are right for you</p>
+					</div>
+				</div>
+				<div className='border rounded-md mx-32 mb-4 bg-orange-200 border-2 border-gray-400'>
+					<div className='mb-4 mt-4 inline-block'>
+						<img src="images/communicationicon.png" className='font-bold text-2xl text-white w-16 ml-6 mr-6 '/>	
+					</div>
+					<div className='inline-block text-2xl xl:text-xl'>
+						<p>
+							Your agents will reach out to you within X business days with options to save you money
+							and better your financial wellness
+						</p>
+					</div>
+				</div>
+			</div>
 			{/* RESOURCES SECTION */}
-			<section className='text-white bg-blue-900'>
+			<section className='text-black bg-stone-100'>
 				<div className='max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8'>
 					<div className='max-w-lg mx-auto text-center'>
 						<h2 className='text-3xl font-bold sm:text-4xl'>Learn More</h2>
 
-						<p className='mt-4 text-gray-300'>Checkout Our Free Learning Resources</p>
+						<p className='mt-4 text-gray-600'>Checkout Our Free Learning Resources</p>
 					</div>
 
 					<div className='flex-col flex md:flex-row justify-space-between justify-center items-center mt-8'>
 						{/* CATEGORY 1*/}
-						<div className='m-3'>
+						<div className='m-3 '>
 							<a
-								className='block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-blue-200/10 hover:border-blue-100/10'
+								className='block p-8 transition bg-green-600 border border-gray-800 shadow-xl rounded-xl hover:shadow-blue-200/10 hover:border-blue-100/10'
 								href='/'>
-								<h3 className='m-4 text-xl font-bold text-white text-center'>Category 1</h3>
+								<h3 className='m-4 text-xl font-bold text-black text-center'>Category 1</h3>
 							</a>
 						</div>
 
 						{/* CATEGORY 2 */}
 						<div className='m-3'>
 							<a
-								className='block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-blue-200/10 hover:border-blue-100/10'
+								className='block p-8 transition bg-orange-600 border border-gray-800 shadow-xl rounded-xl hover:shadow-blue-200/10 hover:border-blue-100/10'
 								href='/'>
-								<h3 className='m-4 text-xl font-bold text-white text-center'>Category 2</h3>
+								<h3 className='m-4 text-xl font-bold text-black text-center'>Category 2</h3>
 							</a>
 						</div>
 
 						{/* CATEGORY 3 */}
 						<div className='m-3'>
 							<a
-								className='block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-blue-200/10 hover:border-blue-100/10'
+								className='block p-8 transition bg-purple-600 border border-gray-800 shadow-xl rounded-xl hover:shadow-blue-200/10 hover:border-blue-100/10'
 								href='/'>
-								<h3 className='m-4 text-xl font-bold text-white text-center'>Category 3</h3>
+								<h3 className='m-4 text-xl font-bold text-black text-center'>Category 3</h3>
 							</a>
 						</div>
 
 						{/* CATEGORY 4 */}
 						<div className='m-3'>
 							<a
-								className='block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-blue-200/10 hover:border-blue-100/10'
+								className='block p-8 transition bg-yellow-600 border border-gray-800 shadow-xl rounded-xl hover:shadow-blue-200/10 hover:border-blue-100/10'
 								href='/'>
-								<h3 className='m-4 text-xl font-bold text-white text-center'>Category 4</h3>
+								<h3 className='m-4 text-xl font-bold text-black text-center'>Category 4</h3>
 							</a>
 						</div>
-					</div>
-
-					<div className='mt-8 text-center'>
-						<a
-							className='inline-flex items-center px-8 py-3 mt-8 text-black bg-white border border-gray-600 rounded hover:bg-transparent active:text-blue-500 focus:outline-none focus:ring'
-							href='/get-started'>
-							<span className='text-sm font-medium'> Get Started </span>
-
-							<svg
-								className='w-5 h-5 ml-3'
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke='currentColor'>
-								<path
-									strokeLinecap='round'
-									strokeLinejoin='round'
-									strokeWidth='2'
-									d='M17 8l4 4m0 0l-4 4m4-4H3'
-								/>
-							</svg>
-						</a>
 					</div>
 				</div>
 			</section>
