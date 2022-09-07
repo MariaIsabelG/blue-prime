38445bcf95de5a521b813d8ee110984a2861d668
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-09-07 15:36:06 -0500

    styled log in page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
-import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LoginPage() {
+  
 	const history = useHistory();  
 
   return (
